23cbbe10c059f71fccc481fd6ca8b52799a3c32c
swastiraut <swastikaraut@gmail.com>
2020-05-24 09:20:40 -0500

    my first commit

---
+++ untitled/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int i =5;
+        int j = 10;
+        System.out.println(i+j);
+
+
+
+
+
+
+    }
+}
