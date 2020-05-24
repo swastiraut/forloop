c9c3dd9c292cc1f4b44ff8af6d325023e86365a3
swastiraut <swastikaraut@gmail.com>
2020-05-24 09:50:01 -0500

    adding two numbers

--- untitled/src/com/company/AddingNumbers.java
+++ untitled/src/com/company/AddingNumbers.java
@@ -1,4 +1,14 @@
 package com.company;
 
 public class AddingNumbers {
+    public static void main(String[] args) {
+        int i = 5;
+        int j = 10;
+        int k = i+j;
+
+        System.out.println(k);
+    }
+
+
+
 }
